cbaa435173e5eb48f6cbccce6e2ce346ada6e618
MahneeraAzam <minheraazam403@gmail.com>
2025-04-15 13:52:39 +0500

    sync

--- main.py
+++ main.py
@@ -2,11 +2,4 @@ print("hello Mindi")
 print('hello again mindi')
 print('hello again mindi')
 print('hello again mindi')
-<<<<<<< HEAD
 print('hello again mindi')
-=======
-print('hello again mindi')
-print('hello again mindi, and this is final helloooooo')
-print('yeah so I swear, hello again mindi, and this is final helloooooo')
-print('hello again mindi, and this is final helloooooo, final changes of V 2.0')
->>>>>>> 84fe9abd1908acc51610c948a97de5c467d49f02
